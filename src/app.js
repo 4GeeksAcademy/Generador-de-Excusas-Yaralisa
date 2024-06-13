ba44a52c57d7c2f6c59a7fa7af33786b8eddccc0
Yaralisa <yaralisa28@gmail.com>
2024-06-13 19:35:42 +0000

    Proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#ExcuseMe").innerHTML = GeneradordeExcusas();
+  });
+  document.querySelector("#ExcuseMe").innerHTML = GeneradordeExcusas();
   console.log("Hello Rigo from the console!");
+};
+let GeneradordeExcusas = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoI = Math.floor(Math.random() * who.length);
+  let actionI = Math.floor(Math.random() * action.length);
+  let whatI = Math.floor(Math.random() * what.length);
+  let whenI = Math.floor(Math.random() * when.length);
+  return (
+    who[whoI] + " " + action[actionI] + " " + what[whatI] + " " + when[whenI]
+  );
 };
